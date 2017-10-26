2f8ff74df8ec3a578545ad861e93b309822a3c67
online5 <online5@ucione.local>
2017-10-26 21:06:51 +0200

    migracije, modeli i view prikaz svih recepata

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
 <div class="container">
 	<div class="col-md-10 col-md-offset-1">
 		<div class="panel panel-default">
-			<div class="panel-heading">Recepti<br>
+			<div class="panel-heading">Recepti<br><br>
 				<a href="{{ url('/recipes/add') }}">
 					<i class="fa fa-btn fa-plus"></i>
 					Dodaj novi recept
