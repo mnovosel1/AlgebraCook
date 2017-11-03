62c7223e6be4c52f09479580a0122609b8726271
online5 <online5@ucione.local>
2017-11-03 20:59:45 +0100

    menu za login, odjavu i profil na master blade

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,16 +12,20 @@
 */
 
 // Authentication routes
-Route::get('/login', 'Auth\AuthController@getLogin');
-Route::post('/login', 'Auth\AuthController@postLogin');
-Route::get('/logout', 'Auth\AuthController@getLogout');
+Route::get('/auth/login', 'Auth\AuthController@getLogin');
+Route::post('/auth/login', 'Auth\AuthController@postLogin');
+Route::get('/auth/logout', 'Auth\AuthController@getLogout');
 
 // Registration routes
-Route::get('/register', 'Auth\AuthController@getRegister');
-Route::post('/register', 'Auth\AuthController@postRegister');
+Route::get('/auth/register', 'Auth\AuthController@getRegister');
+Route::post('/auth/register', 'Auth\AuthController@postRegister');
 
 
 Route::get('/', function () {
+    return redirect('/recipes');
+});
+
+Route::get('/home', function () {
     return redirect('/recipes');
 });
 
